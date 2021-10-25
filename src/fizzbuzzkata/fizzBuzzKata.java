ca902077a146a7a4d89adf30a85d1d758f4f74f6
Ilja Filatov <62281276+P0werPuff@users.noreply.github.com>
2021-10-25 12:27:17 +0300

    Add method fizzbuzzkata

--- src/fizzbuzzkata/fizzBuzzKata.java
+++ src/fizzbuzzkata/fizzBuzzKata.java
@@ -5,4 +5,19 @@ public class fizzBuzzKata {
 	public int[] getNumbers() {
 		return new int[100];
 	}
+	
+	public String fizzbuzzkata(int value) {
+		if (value % 3 == 0 || value % 5 == 0) {
+			if (value % 3 == 0 && value % 5 == 0) {
+				return "fizzbuzz";
+			} else if (value % 3 == 0) {
+				return "fizz";
+			} else if (value % 5 == 0) {
+				return "buzz";
+			}
+		} else {
+			return Integer.toString(value);
+		}
+		return "";
+	}
 } 
